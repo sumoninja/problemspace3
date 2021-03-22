107497938e863a15d51250a3aef10b6e08a99967
sumoninja <michelle.kaleio@gmailcom>
2021-03-22 03:51:34 -0400

    printed ratings and put into function

--- build site selenium.py
+++ build site selenium.py
@@ -14,66 +14,76 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-#driver = webdriver.Chrome(PATH)
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
-link = "https://www.build.com/delta-9159-dst/s380199?uid=2794656"
-driver.get(link)
-time.sleep(5)
+def scrape_build():
+        
 
-print(driver.title)
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+        #driver = webdriver.Chrome(PATH)
+        driver = webdriver.Chrome(ChromeDriverManager().install())
 
-close_popup_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div/div/div/div/div[1]/div") #this code finds the button to close that annoying popup and clicks it
-close_popup_button.click()
+        link = "https://www.build.com/delta-9159-dst/s380199?uid=2794656"
+        driver.get(link)
+        time.sleep(5)
 
-time.sleep(5)
+        print(driver.title)
 
-# open up reviews expandable
-#expand = driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/div[3]/section[3]/div/div/div[2]/div/div/div[1]/div") 
+        close_popup_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div/div/div/div/div[1]/div") #this code finds the button to close that annoying popup and clicks it
+        close_popup_button.click()
 
-expand = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section") #this code finds the button to expand the reviews section
+        time.sleep(5)
 
-#print(expand)
+        # open up reviews expandable
+        #expand = driver.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/div[3]/section[3]/div/div/div[2]/div/div/div[1]/div") 
 
+        expand = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section") #this code finds the button to expand the reviews section
 
-#button = driver.find_element_by_xpath("/html/body/div[2]/div/main/div[1]/section[4]/div[2]/section/div[1]")
-
-#print(button)
-
-
-expand.click() #this code clicks the button to expand the reviews section
-
-time.sleep(5)
-
-# find element by link text
-#reviews = driver.find_element_by_id("review-content")
-
-reviews = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section/div[3]/div/div/div/div/div/div/ol") #the reviews in the reviews section
-#are stored in an unordered list HTML tag. This code finds that unordered list.
+        #print(expand)
 
 
-time.sleep(5)
+        #button = driver.find_element_by_xpath("/html/body/div[2]/div/main/div[1]/section[4]/div[2]/section/div[1]")
 
-more_reviews_button = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section/div[3]/div/div/div/div/div/div/div[4]/div/button")
-#there is also a button to load more reviews. This code finds the button and the code directly below clicks on it
+        #print(button)
 
 
-more_reviews_button.click()
+        expand.click() #this code clicks the button to expand the reviews section
 
-time.sleep(5)
+        time.sleep(5)
 
-individual_reviews = reviews.find_elements_by_tag_name("li")
-#within that unordered list we found above, this line finds all elements with the tag <li>. A <li> tag represents an item in an <ol> (unordered list) tag.
-#so we're basically just finding all the reviews in the list.
+        # find element by link text
+        #reviews = driver.find_element_by_id("review-content")
 
-print("Attempting to access individual reviews...")
+        reviews = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section/div[3]/div/div/div/div/div/div/ol") #the reviews in the reviews section
+        #are stored in an unordered list HTML tag. This code finds that unordered list.
 
-for review in individual_reviews: #iterate through all the reviews we found
-	paragraphs = review.find_elements_by_tag_name("p") #within each review, find a <p> tag.
-	for p in paragraphs: #for each paragraph tag, print its text.
-		print(p.text)
 
+        time.sleep(5)
+
+        more_reviews_button = driver.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/section[4]/div[2]/section/div[3]/div/div/div/div/div/div/div[4]/div/button")
+        #there is also a button to load more reviews. This code finds the button and the code directly below clicks on it
+
+
+        more_reviews_button.click()
+
+        time.sleep(5)
+
+        individual_reviews = reviews.find_elements_by_tag_name("li")
+        #within that unordered list we found above, this line finds all elements with the tag <li>. A <li> tag represents an item in an <ol> (unordered list) tag.
+        #so we're basically just finding all the reviews in the list.
+
+        print("Attempting to access individual reviews...")
+
+        
+        for review in individual_reviews: #iterate through all the reviews we found
+                paragraphs = review.find_elements_by_tag_name("p") #within each review, find a <p> tag.
+                for p in paragraphs: #for each paragraph tag, print its text.
+                        print(p.text)
+        
+        # print _ out of 5 stars for each review
+        for star in individual_reviews:
+                ratings = star.find_elements_by_class_name("bv-rating-stars-container")
+                for r in ratings:
+                        print(r.text)
 
 #####################################################################
 '''
